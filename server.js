d7145a01b1ca18375dd0fca66ab14c627c17775b
nooriaali9 <30815688+nooriaali9@users.noreply.github.com>
2017-08-21 19:22:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.get('/', function (req, res) {
 });
 app.get('/article-one ', function (req,res){
     res.send('we like to party');
-    })
+    });
     app.get('/article-two ', function (req,res){
     res.send('we like to party party ');
-    })
+    });
     app.get('/article-three ', function (req,res){
     res.send('we like to party party party');
-    })
+    });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
